72516ac29bbbf8f0c126fea6ee55d91fd22fe813
Joe Mendez <joejmendez@gmail.com>
2026-09-09 13:33:04 -0700

    fixed staff name in business dashboard

--- app.py
+++ app.py
@@ -2029,7 +2029,7 @@ class Invite(db.Model):
 class Staff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     business_id = db.Column(db.Integer, db.ForeignKey('business.id'), nullable=False)
-    name = db.Column(db.String(255)),
+    name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, nullable=False)
     hashed_password = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(20), default="staff")  # Future roles possible
